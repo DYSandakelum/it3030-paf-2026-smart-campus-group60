e676226c665af45372997f20b195f97008080597
Yasas Sandakelum <yasassandakelum123@gmail.com>
2026-04-24 12:51:31 +0530

    Revert "Feature/bookings"

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,3 @@
-import React from "react";
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import Navbar from "./components/Navbar.jsx";
-import BookingList from "./pages/BookingList.jsx";
-import CreateBooking from "./pages/CreateBooking.jsx";
-import BookingDetail from "./pages/BookingDetail.jsx";
-import AdminBookings from "./pages/AdminBookings.jsx";
-
-function App() {
-  return (
-    <BrowserRouter>
-      <div className="app-shell">
-        <Navbar />
-        <main className="app-main">
-          <Routes>
-            <Route path="/" element={<BookingList />} />
-            <Route path="/create" element={<CreateBooking />} />
-            <Route path="/booking/:id" element={<BookingDetail />} />
-            <Route path="/admin" element={<AdminBookings />} />
-            <Route path="*" element={<Navigate to="/" replace />} />
-          </Routes>
-        </main>
-      </div>
-    </BrowserRouter>
-  );
-}
 import { BrowserRouter, Routes, Route, Navigate }
   from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
